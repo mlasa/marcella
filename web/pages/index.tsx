025c9a07725b93c1dfdf1e683cffd4e3aadb8bce
Marcella LASA <marcella.amorimsa@gmail.com>
2021-04-23 18:12:11 -0300

    config: create web project - create web project with next - install typescript and sass - change file extesions to tsx, tsx and scss

---
+++ web/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <h1 className={styles.title}>Hello Marcella</h1>
+  )
+}
