9e281dc1c5857e5df2eaccc4de9418412b321044
Marcella LASA <marcella.amorimsa@gmail.com>
2021-04-24 16:09:42 -0300

    feat:create basic home page

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Merriweather+Sans:wght@400;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,44 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello Marcella</h1>
+    <>
+      <Head>
+        <title>Marcella L.A.S.A.</title>
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <div className={styles.me}>
+            <img src="https://avatars.githubusercontent.com/u/43733159?s=400&u=44809dcf2f7daef870a8404e63973b5519be6a5a&v=4" alt="Foto de Marcella" />
+            <h2>Marcella Letícia de A.S. Ananias</h2>
+            <strong>Estudante de programação</strong>
+            <span>
+              <p>
+                Formada em Sistemas de Informação desde dezembro de 2020, e estudando programação para trabalhar como desenvolvedora.
+                Meu foco tem sido em desenvolvimento web, mais especificamente com ReactJS e NodeJS, e coloco meus estudos em prática com projetos,
+                que eu coloco no meu GitHub.
+              </p>
+            </span>
+          </div>
+          <span className={styles.socialNetwork}>
+            <Link href="https://www.linkedin.com/in/marcellaamorim/">
+              <strong>
+                <AiFillLinkedin size={30} />
+                LinkedIn
+              </strong>
+            </Link>
+            <Link href="https://github.com/mlasa">
+              <strong>
+                <AiFillGithub size={30} />
+                GitHub
+              </strong>
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
   )
 }
