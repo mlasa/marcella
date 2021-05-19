92bf84bebc65d103837998966113196ff3898511
Marcella LASA <marcella.amorimsa@gmail.com>
2021-05-19 08:42:25 -0300

    styles: height and width

--- web/src/pages/projetos/index.tsx
+++ web/src/pages/projetos/index.tsx
@@ -1,13 +1,43 @@
-import styles from './projetos.module.scss'
+import {GetServerSideProps} from 'next'
+import api_git from '../../services/api_github'
 import { useTheme } from '../../hooks/theme'
+import styles from './projetos.module.scss'
 
-function Projects() {
+interface Repository{
+  name: string;
+  html_url: string;
+  description: string;
+}
+interface ProjectsProps{
+  repository : Repository;
+}
+
+export default function Projects(props: ProjectsProps) {
   const { globalTheme } = useTheme()
+  const {repository} = props
+
   return (
     <div className={`${styles.projectsContainer} ${styles[globalTheme]}`}>
-      <h1>Projetos</h1>
+      <h1>Projetos[em desenvolvimento]</h1>
+        <div>
+          <p>{repository.name}</p>
+          <p>{repository.html_url}</p>
+          <p>{repository.description}</p>
+        </div>
     </div>
   )
 }
 
-export default Projects
+
+export const getServerSideProps = async (ctx)=> {
+  console.log('opa nene')
+  const response = await api_git.get('/mlasa/marcella')
+  console.log('opaa===============================================',response.data)
+
+  return {
+    props:{
+      repository: response.data
+    }
+  }
+
+};
--- web/src/services/api_github.ts
+++ web/src/services/api_github.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api_git = axios.create({
-  baseURL: 'https://api.github.com'
+  baseURL: 'https://api.github.com/repos'
 })
 export default api_git
