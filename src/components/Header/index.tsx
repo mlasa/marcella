8d96efb7d49e2d3b253f473af25868689e2c2ec0
Marcella Amorim <marcella.amorimsa@gmail.com>
2021-09-12 13:49:51 -0300

    [FEAT][ADD] - Tornar header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,11 +9,27 @@ import {
   PopoverArrow,
   PopoverCloseButton,
 } from "@chakra-ui/react"
-import { extendTheme } from "@chakra-ui/react"
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+  IconButton
+} from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons"
+import { useMediaQuery } from "@chakra-ui/react"
 
 import styles from './styles.module.scss'
 
 export default function Header() {
+  const [isLargerThan750px] = useMediaQuery("(min-width: 750px)")
+
 
   return (
     <>
@@ -22,22 +38,46 @@ export default function Header() {
         ${styles.headerContainer}  
     `}
       >
+        {
+          !isLargerThan750px &&
+          <div className={styles.menuHamburger}>
+            <Menu isLazy>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+                style={{ background: 'transparent' }}
+              />
+              <MenuList>
+                <div className={styles.menuList}>
+                  <MenuItem>
+                    <Link href="/sobre">Sobre</Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link href="/experiencias">Experiências</Link>
+                  </MenuItem>
+                </div>
+              </MenuList>
+            </Menu>
+          </div>
+        }
         <h1>Developer</h1>
         <div className={styles.linksTabs}>
-          <p>
-            <Link href="/sobre">Sobre</Link>
-          </p>
-          <p>
-            <Link href="/experiencias">Experiências</Link>
-          </p>
+          {isLargerThan750px &&
+            <>
+              <p>
+                <Link href="/sobre">Sobre</Link>
+              </p>
+              <p>
+                <Link href="/experiencias">Experiências</Link>
+              </p>
+            </>
+          }
         </div>
         <Popover>
           <PopoverTrigger>
             <a href="#" className={styles.neonLink}>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
               <Button
                 colorScheme="yellow"
                 variant="outline"
