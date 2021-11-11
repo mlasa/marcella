b664bca50327ddc6ca296be5670eb6db04501ca2
Marcella <marcella.amorimsa@gmail.com>
2021-11-11 10:09:02 -0300

    [FEAT]- pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,16 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel="manifest" href="/manifest.json" />
+
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet" />
 
 					<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"></link>
+					<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+					<meta name="theme-color" content="#317EFB" />
+
 				</Head>
 				<title>Marcella Dev</title>
 				<body>
