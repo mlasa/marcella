a86f1118dd544a5d9565607dbb2504b31d54d4bd
Marcella A.S.A <marcella.amorimsa@gmail.com>
2022-06-11 18:44:33 -0300

    feat: adicionar url da api para producao e para local

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,109 +9,109 @@ import styles from './styles.module.scss'
 import api from '../../services/api'
 
 export default function Login() {
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
-    const router = useRouter()
-    const toast = useToast()
+	const [password, setPassword] = useState('')
+	const [email, setEmail] = useState('')
+	const router = useRouter()
+	const toast = useToast()
 
-    function saveUserLogged(token, user): void {
-        Cookie.set('@mlasaPortfolio', JSON.stringify({ token, user }), {
-            expires: addDays(new Date(), 1)
-        })
-    }
+	function saveUserLogged(token, user): void {
+		Cookie.set('@mlasaPortfolio', JSON.stringify({ token, user }), {
+			expires: addDays(new Date(), 1)
+		})
+	}
 
-    function login(email, password): void {
+	function login(email, password): void {
 
-        !email && !password &&
-            toast({
-                title: "Você precisa informar as credenciais",
-                description: "Preecha senha e e-mail",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-            })
-
-
-        api.post('/session', { email: email, password: password })
-            .then(response => {
-                const { token, user } = response.data
-
-                if (token && user) {
-                    saveUserLogged(token, user)
-                    router.push('/dashboard')
-                }
+		!email && !password &&
+			toast({
+				title: "Você precisa informar as credenciais",
+				description: "Preecha senha e e-mail",
+				status: "error",
+				duration: 9000,
+				isClosable: true,
+			})
 
 
-                toast({
-                    title: "Login feito com sucesso",
-                    status: "success",
-                    duration: 9000,
-                    isClosable: true,
-                })
-            })
-            .catch(error => {
-                console.log(error)
-                toast({
-                    title: error.response.data.errorMessage,
-                    status: "error",
-                    duration: 9000,
-                    isClosable: true,
-                })
-            })
-    }
+		api.post('/session', { email: email, password: password })
+			.then(response => {
+				const { token, user } = response.data
 
-    function checkIfUserIsLogged(): Boolean {
-        const userLoggedIn = Cookie.get('@mlasaPortfolio')
+				if (token && user) {
+					saveUserLogged(token, user)
+					router.push('/dashboard')
+				}
 
-        if (userLoggedIn)
-            return true
-        else return false
-    }
 
-    useEffect(() => {
-        if (checkIfUserIsLogged())
-            router.push('/dashboard')
-    }, [])
+				toast({
+					title: "Login feito com sucesso",
+					status: "success",
+					duration: 9000,
+					isClosable: true,
+				})
+			})
+			.catch(error => {
+				console.log(error)
+				toast({
+					title: error.response ? error.response.data.errorMessage : "Erro",
+					status: "error",
+					duration: 9000,
+					isClosable: true,
+				})
+			})
+	}
 
-    return (
-        <>
-            <Head>
-                <title> Marcella Dev | Login</title>
-            </Head>
+	function checkIfUserIsLogged(): Boolean {
+		const userLoggedIn = Cookie.get('@mlasaPortfolio')
 
-            <div className={styles.loginContainer}>
-                <section className={`${styles.sectionLogin}`}>
+		if (userLoggedIn)
+			return true
+		else return false
+	}
 
-                    <Heading size="lg">Acesso ao painel</Heading>
-                    <div className={styles.boxLogin}>
-                        <Input
-                            value={email}
-                            variant="outline"
-                            placeholder="E-mail ou nome de usuário"
-                            onChange={(event) => setEmail(event.target.value)}
-                            autoFocus={true}
-                        />
-                        <Input
-                            value={password}
-                            variant="outline"
-                            type="password"
-                            placeholder="*****"
-                            onChange={(event) => setPassword(event.target.value)}
-                        />
-                        {email && password ?
-                            <Button
-                                onClick={() => login(email, password)}
-                                colorScheme="cyan"
-                            >
-                                Entrar
-                            </Button>
-                            :
-                            <Button disabled colorScheme="cyan">Entrar</Button>
-                        }
-                    </div>
+	useEffect(() => {
+		if (checkIfUserIsLogged())
+			router.push('/dashboard')
+	}, [])
 
-                </section>
-            </div >
-        </>
-    )
+	return (
+		<>
+			<Head>
+				<title> Marcella Dev | Login</title>
+			</Head>
+
+			<div className={styles.loginContainer}>
+				<section className={`${styles.sectionLogin}`}>
+
+					<Heading size="lg">Acesso ao painel</Heading>
+					<div className={styles.boxLogin}>
+						<Input
+							value={email}
+							variant="outline"
+							placeholder="E-mail ou nome de usuário"
+							onChange={(event) => setEmail(event.target.value)}
+							autoFocus={true}
+						/>
+						<Input
+							value={password}
+							variant="outline"
+							type="password"
+							placeholder="*****"
+							onChange={(event) => setPassword(event.target.value)}
+						/>
+						{email && password ?
+							<Button
+								onClick={() => login(email, password)}
+								colorScheme="cyan"
+							>
+								Entrar
+							</Button>
+							:
+							<Button disabled colorScheme="cyan">Entrar</Button>
+						}
+					</div>
+
+				</section>
+			</div >
+		</>
+	)
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
+const localApi = 'http://localhost:3333';
+const productionApi = 'https://mypagemlasa.herokuapp.com';
+
 const api = axios.create({
-  baseURL: 'https://mypagemlasa.herokuapp.com',
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    "Access-Control-Allow-Methods": "OPTIONS, DELETE, POST, GET, PATCH, PUT",
-    "Access-Control-Allow-Credentials": "true"
-  }
+	baseURL: localApi,
+	headers: {
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+		"Access-Control-Allow-Methods": "OPTIONS, DELETE, POST, GET, PATCH, PUT",
+		"Access-Control-Allow-Credentials": "true"
+	}
 })
 export default api
