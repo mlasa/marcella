d3fde151356f77160ae034ef4ae433178fbe3a15
Marcella A.S.A <marcella.amorimsa@gmail.com>
2022-06-11 20:07:44 -0300

    feat: adicionar campo de username e email no dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,88 +4,91 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Cookie from "js-cookie"
 import {
-    Button,
-    UnorderedList,
-    ListItem,
-    Input,
-    Textarea,
-    Stack,
-    Heading
+	Button,
+	UnorderedList,
+	ListItem,
+	Input,
+	Textarea,
+	Stack,
+	Heading
 } from "@chakra-ui/react"
 
 import styles from './styles.module.scss'
 
 interface IUser {
-    _id: string;
-    username: string;
-    name: string;
-    email: string;
+	_id: string;
+	username: string;
+	name: string;
+	email: string;
 }
 
 export default function Dashboard() {
-    const router = useRouter()
-    const [user, setUser] = useState<IUser>({} as IUser)
+	const router = useRouter()
+	const [user, setUser] = useState<IUser>({} as IUser)
 
-    function logOut(): void {
-        Cookie.remove('@mlasaPortfolio')
-        router.push('/login')
-    }
+	function logOut(): void {
+		Cookie.remove('@mlasaPortfolio')
+		router.push('/login')
+	}
 
-    // Will check if user is already logged in by client side
-    useEffect(() => {
-        const userLoggedIn = Cookie.get('@mlasaPortfolio')
+	// Will check if user is already logged in by client side
+	useEffect(() => {
+		const userLoggedIn = Cookie.get('@mlasaPortfolio')
 
-        if (!userLoggedIn) {
-            router.push('/login')
-        }
-        else {
-            setUser(JSON.parse(userLoggedIn).user)
-        }
-    }, [])
+		if (!userLoggedIn) {
+			router.push('/login')
+		}
+		else {
+			setUser(JSON.parse(userLoggedIn).user)
+		}
+	}, [])
 
-    return (
-        <>
-            <Head>
-                <title> Marcella Dev | Dashboard</title>
-            </Head>
+	return (
+		<>
+			<Head>
+				<title> Marcella Dev | Dashboard</title>
+			</Head>
 
-            <div className={styles.dashboardContainer}>
-                <div className={styles.header}>
-                    <h1>Olá {user?.name?.split(' ')[0]}</h1>
-                    <Button
-                        onClick={logOut}
-                        size="sm"
-                        variant="link"
-                    >Sair</Button>
-                </div>
+			<div className={styles.dashboardContainer}>
+				<div className={styles.header}>
+					<h1>Olá {user?.name?.split(' ')[0]}</h1>
+					<Button
+						onClick={logOut}
+						size="sm"
+						variant="link"
+					>Sair</Button>
+				</div>
 
-                <div className={styles.wrapperContent}>
-                    <section className={styles.listSection}>
-                        <UnorderedList className={styles.list}>
-                            <ListItem>Editar informações do portfólio</ListItem>
-                            <ListItem>
-                                <Link href="/">Ver portfólio</Link>
-                            </ListItem>
-                        </UnorderedList>
-                    </section>
+				<div className={styles.wrapperContent}>
+					<section className={styles.listSection}>
+						<UnorderedList className={styles.list}>
+							<ListItem>Editar informações do portfólio</ListItem>
+							<ListItem>
+								<Link href="/">Ver portfólio</Link>
+							</ListItem>
+						</UnorderedList>
+					</section>
 
-                    <section className={styles.formWrapper}>
-                        <div>
-                            <form>
-                                <Stack spacing={2}>
-                                    <Heading size="md" style={{ marginBottom: '1rem' }}>Informações na homepage</Heading>
-                                    <Input variant="filled" placeholder="Nome" value={user.name || ''} onChange={(e) => setUser({ ...user, name: e.target.value })} />
-                                    <Input variant="filled" placeholder="Cargo" />
-                                    <Textarea
-                                        variant="filled"
-                                        placeholder="O que mais tem a contar?"
-                                    />
-                                </Stack>
-                            </form>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </>
-    )
+					<section className={styles.formWrapper}>
+						<div>
+							<form>
+								<Stack spacing={2}>
+									<Heading size="md">Informações na homepage</Heading>
+									<Input variant="filled" placeholder="Nome" value={user.name || ''} onChange={(e) => setUser({ ...user, name: e.target.value })} />
+									<Input variant="filled" placeholder="Cargo" />
+									<Textarea
+										variant="filled"
+										placeholder="O que mais tem a contar?"
+									/>
+									<Heading size="md" style={{ marginTop: '2rem' }}>Dados de usuário</Heading>
+									<Input variant="filled" placeholder="Usuário" value={user.username || ''} onChange={(e) => setUser({ ...user, username: e.target.value })} />
+									<Input variant="filled" placeholder="E-mail" value={user.email || ''} onChange={(e) => setUser({ ...user, email: e.target.value })} />
+								</Stack>
+							</form>
+						</div>
+					</section>
+				</div>
+			</div>
+		</>
+	)
 }
