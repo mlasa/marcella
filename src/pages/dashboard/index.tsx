b2b790f9e09ab0da8ef53e2771d77ea38980f5c0
Marcella A.S.A <marcella.amorimsa@gmail.com>
2022-06-12 19:36:51 -0300

    feat: formulario para edicao da home e dados do usuario

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -10,10 +10,12 @@ import {
 	Input,
 	Textarea,
 	Stack,
-	Heading
-} from "@chakra-ui/react"
+	Heading,
+	Divider
+} from "@chakra-ui/react";
 
-import styles from './styles.module.scss'
+import api from "../../services/api";
+import styles from './styles.module.scss';
 
 interface IUser {
 	_id: string;
@@ -22,13 +24,37 @@ interface IUser {
 	email: string;
 }
 
+interface IProfile {
+	description: string;
+	tags: Array<string>;
+	name: string;
+	job?: string;
+}
+
 export default function Dashboard() {
 	const router = useRouter()
 	const [user, setUser] = useState<IUser>({} as IUser)
+	const [profile, setProfile] = useState<IProfile>({} as IProfile);
+	console.log('profile: ', profile);
 
 	function logOut(): void {
 		Cookie.remove('@mlasaPortfolio')
 		router.push('/login')
+	}
+
+	async function getUser(): any {
+		const responseRaw = await api({
+			url: "/profile",
+			method: "GET"
+		})
+		return responseRaw.data
+	}
+
+	function removeTag(e, indexTag) {
+		e.preventDefault()
+
+		const newArray = profile.tags.filter((tag, index) => index !== indexTag)
+		setProfile({ ...profile, tags: newArray });
 	}
 
 	// Will check if user is already logged in by client side
@@ -41,6 +67,12 @@ export default function Dashboard() {
 		else {
 			setUser(JSON.parse(userLoggedIn).user)
 		}
+
+		getUser().then(response => {
+			response && setProfile(response[0]);
+		});
+
+
 	}, [])
 
 	return (
@@ -72,27 +104,73 @@ export default function Dashboard() {
 					<section className={styles.formWrapper}>
 						<div>
 							<form>
-								<Stack spacing={2}>
-									<Heading size="md">Informações na homepage</Heading>
-									<Input variant="filled" placeholder="Nome" value={user.name || ''} onChange={(e) => setUser({ ...user, name: e.target.value })} />
-									<Input variant="filled" placeholder="Cargo" />
-									<Button colorScheme='green' size='md'>
-										Salvar
-									</Button>
+								<div className={styles.section}>
+									<Heading size="md">Informações da home</Heading>
+									<Stack spacing={2}>
+										<Input variant="filled" placeholder="Nome" value={profile.name || ''} onChange={(e) => setProfile({ ...profile, name: e.target.value })} />
+										<Input variant="filled" placeholder="Cargo"
+											value={profile.job || ""}
+											onChange={(e) => {
+												if (!profile.hasOwnProperty("job")) {
+													Object.assign(profile, { ...profile, job: e.target.value })
+													console.log('profile: ', profile);
 
-									<Heading size="md" style={{ marginTop: '2rem' }}>Dados de usuário</Heading>
-									<Input variant="filled" placeholder="Usuário" value={user.username || ''} onChange={(e) => setUser({ ...user, username: e.target.value })} />
-									<Input variant="filled" placeholder="E-mail" value={user.email || ''} onChange={(e) => setUser({ ...user, email: e.target.value })} />
-									<Input variant="filled" placeholder="Nova senha" value={''} />
-									<Button colorScheme='green' size='md'>
-										Salvar
-									</Button>
-								</Stack>
+												} else {
+													setProfile({ ...profile, job: e.target.value })
+												}
+
+											}}
+										/>
+										<Textarea
+											variant="filled"
+											placeholder='Descrição'
+											size='sm'
+											onChange={(e) => setProfile({ ...profile, description: e.target.value })}
+											value={profile.description || ""}
+										/>
+
+										{/* <Button colorScheme='green' size='md'>
+												Salvar
+											</Button> */}
+										<Divider />
+										<div className={styles.tags}>
+											<Heading size="md">Palavras chave</Heading>
+											<div className={styles.wrappertags}>
+												{
+													profile.tags &&
+													profile.tags.map((tag, index) => {
+														return <div key={`${tag}${index}`} className={styles.tag}>
+															<p>{tag}</p>
+															<button type="button" onClick={(e) => removeTag(e, index)}>X</button>
+														</div>;
+													})
+												}
+											</div>
+										</div>
+									</Stack>
+								</div>
+
+								<Divider />
+
+								<div className={styles.section}>
+									<Heading size="md">Dados de usuário</Heading>
+									<Stack spacing={2}>
+										<Input variant="filled" placeholder="Usuário" value={user.name || ''} onChange={(e) => setUser({ ...user, name: e.target.value })} />
+										<Input variant="filled" placeholder="Usuário" value={user.username || ''} onChange={(e) => setUser({ ...user, username: e.target.value })} />
+										<Input variant="filled" placeholder="E-mail" value={user.email || ''} onChange={(e) => setUser({ ...user, email: e.target.value })} />
+										{/* <Input variant="filled" placeholder="Nova senha" value={''} /> */}
+										{/* <Button colorScheme='green' size='md'>
+											Salvar
+										</Button> */}
+									</Stack>
+								</div>
 							</form>
 						</div>
 					</section>
+					<small>https://github.com/mlasa/front/</small>
 				</div>
-			</div>
+
+			</div >
 		</>
 	)
 }
